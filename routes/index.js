22a30df4814a1ea3c927ea072610b83598b74197
gillilands19 <gillilands19@gmail.com>
2019-03-08 13:43:15 -0800

    added comments

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,31 @@ router.get('/route1', (req, res, next) => {
   res.render('route1', { title: 'Express' });
 });
 
+// route for returning colors json object
 router.get('/route2', (req, res, next) => {
   return res.json(colorsObj);
 });
 
+// route for specific color using params
 router.get('/route2/:color', (req, res, next) => {
+  // declare empty variable to store a matching color
   let jsonMatch;
-  const singleColor = req.params.color.toLowerCase();
 
+  // gets the color param and returns in lowercase
+  const singleColor = req.params.color.toLowerCase();
+  
+  //  loops through the array of objects to find a matching color
   colorsObj.colors.forEach((color) => {
     if (color.color === singleColor) {
       jsonMatch = color;
     }
   });
-
+  // this is here because if we try to return res.json inside the loop,
+  // it will attempt to set/write the header multiple times
+  // which throws this error: Error [ERR_HTTP_HEADERS_SENT]
+  // So first we find a match, store it in the jsonMatch Variable
+  // if it is defined, we RETURN the res.json so it doesn't continue
+  // executing other middleware or attempt to set the headers again
   if (jsonMatch) {
     return res.json(jsonMatch);
   }
